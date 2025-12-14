6d51cc24aa05d7bb6020a2a9573edbc381a98711
Raymond Insular <lesteraymond9@gmail.com>
2025-12-14 20:02:56 +0800

    cool commit.

---
+++ main.dart
@@ -0,0 +1,74 @@
+void main() {
+  //https://www.youtube.com/watch?v=Ej_Pcr4uC2Q&t=401s
+  //34:50
+  //stdout.write("Enter your name: ");
+  //String? name = stdin.readLineSync();
+
+  //print("Hello, $name!");
+
+  //int a = 200;
+  //var b = a;
+
+  //double c = 2.5;
+  //var d = c;
+
+  //String e = "HANNI";
+  //var f = e;
+
+  //dynamic x = 2;
+  dynamic y = 2;
+
+  //String str1 = "It's easy to escape string delimiter";
+  //String str2 = 'It\'s easy to escape string delimiter';
+  //String str3 = r"It's easy\n to escape string delimiter";
+
+  //print(str3);
+
+  //var age = 45;
+  //var str = "My age is: $age";
+  //print(str);
+
+  //var names = """
+  //HANNI
+  //MINJI
+  //HAERIN
+  //DANIELLE
+  //HYEIN
+  //""";
+  //print(names);
+
+  //var num = int.parse("5");
+  //print(num.isEven);
+
+  //var fixed = double.parse("3.14159").toStringAsFixed(2);
+  //print(fixed);
+
+  //const int num = 15;
+  //print(num.runtimeType);
+  //dynamic x = 10;
+  //print(x.runtimeType);
+  //x = "HANNI";
+  //print(x.runtimeType);
+  //x = false;
+  //print(x.runtimeType);
+  //x = 3.14;
+  //print(x.runtimeType);
+
+  //var x;
+  //print(x);
+
+  //int x = 5;
+  //print(++x);
+
+  //var n;
+  //var number;
+  //number = n?.num ?? 0;
+  //print(number.runtimeType);
+
+  var number;
+  print(number ??= 100);
+}
+
+//class Num {
+//  int num = 10;
+//}
